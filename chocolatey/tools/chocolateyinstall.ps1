0a3301f33f2165623acdee31477c88fd94866ba6
Dan Clarke <dan@dracan.co.uk>
2018-01-21 19:58:50 +0000

    Add Chocolatey files
    Issue #4

---
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,6 @@
+﻿$name = 'Tomatoad'
+$installerType = 'exe'
+$url  = 'https://github.com/dracan/tomatoad/releases/download/0.1.0/Tomatoad.Setup.0.1.0.exe'
+$silentArgs = '/S'
+
+Install-ChocolateyPackage $name $installerType $silentArgs $url
