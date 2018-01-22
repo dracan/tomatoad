f1b6c2d47d12096aa45197c0f2501e00c056f5d1
Dan Clarke <dan@dracan.co.uk>
2018-01-22 21:51:45 +0000

    Updated version info to 0.1.1

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -1,6 +1,1 @@
-﻿$name = 'Tomatoad'
-$installerType = 'exe'
-$url  = 'https://github.com/dracan/tomatoad/releases/download/0.1.0/Tomatoad.Setup.0.1.0.exe'
-$silentArgs = '/S'
-
-Install-ChocolateyPackage $name $installerType $silentArgs $url
+Install-ChocolateyPackage 'Tomatoad' 'exe' '/S' 'https://github.com/dracan/tomatoad/releases/download/0.1.1/Tomatoad.Setup.0.1.0.exe'
